b90e44cd46d5055a59ced10699c0d682cb0b5fad
Suman Adhikari <expertxkid@gmail.com>
2022-09-08 15:00:03 +0545

    refactor: add configs module for DI in main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
+	"golang_rest_api_starter_project/core/configs"
 	"golang_rest_api_starter_project/core/routers"
 )
 
@@ -10,21 +11,26 @@ func main() {
 
 	// region - DI
 	options := fx.Options(
+		configs.Module,
 		routers.Module,
 	)
 	// endregion
 
 	fx.New(
 		options,
-		fx.Invoke(func(router *routers.AppRouter) {
+		fx.Invoke(func(appConfig *configs.AppConfig, router *routers.AppRouter) {
 			// Setup Gin Engine
 			ginEngine := gin.Default()
+
+			// set ginMode using appConfig
+			gin.SetMode(appConfig.GinMode)
 
 			// Register app routes
 			router.RegisterRoutes(ginEngine)
 
 			// RUN API Server on predefined port on
 			ginEngine.Run()
+
 		}),
 	)
 }
