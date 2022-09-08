0d74ae9afa265f02953ccaa8892f9c907f8f5607
Suman Adhikari <expertxkid@gmail.com>
2022-09-08 16:50:59 +0545

    refactor: add v1HomeController

--- v1/feature_name/controllers/controller.go
+++ v1/feature_name/controllers/controller.go
@@ -1,1 +1,29 @@
-package v1Controllers
+package v1HomeControllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/fx"
+	"net/http"
+)
+
+type HomeController struct {
+}
+
+func NewHomeController() *HomeController {
+	return &HomeController{}
+}
+
+var Module = fx.Provide(
+	NewHomeController,
+)
+
+func (homeController *HomeController) RegisterControllerRoutes(rootGroup *gin.RouterGroup) {
+	group := rootGroup.Group("home")
+
+	group.GET("", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "Hello world!",
+		})
+	})
+
+}
